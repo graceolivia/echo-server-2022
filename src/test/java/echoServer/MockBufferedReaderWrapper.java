a3e62b06dfe751b48f526b73e3b07d4cf634bf2b
Grace <gracelidinskysmith@gmail.com>
2022-02-07 12:16:39 -0500

    Partially fixed test

--- src/test/java/echoServer/MockBufferedReaderWrapper.java
+++ src/test/java/echoServer/MockBufferedReaderWrapper.java
@@ -2,18 +2,30 @@ package echoServer;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.List;
+import java.util.Stack;
 
-public class MockBufferedReaderWrapper implements ClientReadable {
+public class MockBufferedReaderWrapper<inputArray> implements ClientReadable {
     private String input;
     public boolean readLineWasCalled = false;
+    private String[] inputArray = input.split("\r\n");
+    Stack<String> inputStack = toStack(inputArray);
 
     public MockBufferedReaderWrapper(String input) {
         this.input = input;
     }
 
-    @Override
+
+
+
     public String readLine() throws IOException {
         readLineWasCalled = true;
-        return input;
+        return inputStack.pop();
+    }
+
+    private Stack<String> toStack(String[] inputArray){
+        Stack<String> inputStack = new Stack<String>();
+        inputStack.addAll(List.of(inputArray));
+        return inputStack;
     }
 }
