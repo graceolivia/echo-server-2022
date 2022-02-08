9bb6c8346e7502ff9b0edcd33e067e6e719b9bbf
Grace <gracelidinskysmith@gmail.com>
2022-02-08 10:41:29 -0500

    WIP

--- src/main/java/echoServer/http/RequestParser.java
+++ src/main/java/echoServer/http/RequestParser.java
@@ -3,8 +3,14 @@ package echoServer.HTTP;
 public class RequestParser {
 
     public static HTTPRequest RequestParser(String rawRequest) {
+        HTTPRequest httpRequest = new HTTPRequest();
+        String[] rows = rawRequest.split("\r\n");
+        String[] firstRequestRow = rows[0].split(" ");
 
-        rawRequest.split("\r\n")
+        httpRequest.requestType = firstRequestRow[0];
+        httpRequest.route = firstRequestRow[1];
+        httpRequest.httpType = firstRequestRow[2];
+        return httpRequest;
 
     }
 
