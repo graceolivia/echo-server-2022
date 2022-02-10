231fa537eb535423df57615f73f8391514264868
Grace <gracelidinskysmith@gmail.com>
2022-02-10 16:17:14 -0500

    Make private functions for ResponseBuilder lines

--- src/main/java/httpServer/http/ResponseBuilder.java
+++ src/main/java/httpServer/http/ResponseBuilder.java
@@ -2,26 +2,44 @@ package httpServer.http;
 
 import java.util.List;
 
+import static httpServer.http.Constants.CRLF;
+
 public class ResponseBuilder {
 
-    public String buildResponse(StatusCodes statusCode, List methods){
+    public String buildResponse(StatusCodes statusCode, List methods, HTTPRequest request){
         StringBuilder responseStringBuilder = new StringBuilder();
-        responseStringBuilder.append("HTTP/1.1 ");
-        responseStringBuilder.append(statusCode.httpResponse);
-        responseStringBuilder.append(Constants.CRLF);
+        responseStringBuilder.append(makeStatusLine(statusCode, request));
         if (!statusCode.equals(StatusCodes.PAGE_NOT_FOUND)) {
-            responseStringBuilder.append("Allow: ");
-            for (int i = 0; i < methods.size(); i++) {
-                responseStringBuilder.append(methods.get(i));
-                if (i < (methods.size() - 1)) {
-                    responseStringBuilder.append(", ");
-                }
-            }
-            responseStringBuilder.append(Constants.CRLF);
+            responseStringBuilder.append(makeAllowLine(methods));
         }
-        responseStringBuilder.append("Content-Length: 0");
-        responseStringBuilder.append(Constants.CRLF);
+        responseStringBuilder.append(makeContentLengthLine());
         return responseStringBuilder.toString();
+    }
+
+    private String makeStatusLine(StatusCodes statusCode, HTTPRequest request) {
+        StringBuilder statusLine = new StringBuilder();
+        statusLine.append(request.httpVersionNumber + " ");
+        statusLine.append(statusCode.httpResponse);
+        statusLine.append(CRLF);
+        return statusLine.toString();
+    }
+
+    private String makeAllowLine(List methods) {
+        StringBuilder allowLine = new StringBuilder();
+        allowLine.append("Allow: ");
+        for (int i = 0; i < methods.size(); i++) {
+            allowLine.append(methods.get(i));
+            if (i < (methods.size() - 1)) {
+                allowLine.append(", ");
+            }
+        }
+        allowLine.append(CRLF);
+
+        return allowLine.toString();
+    }
+
+    private String makeContentLengthLine() {
+    return("Content-Length: 0" + CRLF);
     }
 
 }
