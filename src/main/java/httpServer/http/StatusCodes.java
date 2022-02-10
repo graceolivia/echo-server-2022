d2fa4a794f7f6a9bea41703717c556baebf19f1c
Grace <gracelidinskysmith@gmail.com>
2022-02-10 10:46:46 -0500

    Return 405 to bad request method to existing resource

--- src/main/java/httpServer/http/StatusCodes.java
+++ src/main/java/httpServer/http/StatusCodes.java
@@ -3,7 +3,8 @@ package httpServer.http;
 public enum StatusCodes {
     PAGE_NOT_FOUND("HTTP/1.1 404 Not Found" + constants.CRLF + "Content-Length: 0" + constants.CRLF ),
     BAD_REQUEST("HTTP/1.1 400 Bad Request" + constants.CRLF + "Content-Length: 0" + constants.CRLF),
-    OK("HTTP/1.1 200 OK" + constants.CRLF+ "Content-Length: 0" + constants.CRLF);
+    OK("HTTP/1.1 200 OK" + constants.CRLF + "Content-Length: 0" + constants.CRLF),
+    NOT_ACCEPTABLE("HTTP/1.1 405 Not Acceptable" + constants.CRLF + "Allow: HEAD, OPTIONS" + constants.CRLF + "Content-Length: 0" + constants.CRLF);
 
     public final String httpResponse;
 
