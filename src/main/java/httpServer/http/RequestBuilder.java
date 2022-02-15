bab3da2b5c6e18e40ab5ed227831ded2354aa479
Grace <gracelidinskysmith@gmail.com>
2022-02-15 13:23:27 -0500

    Reconfigure if statements

---
+++ src/main/java/httpServer/http/RequestBuilder.java
@@ -0,0 +1,18 @@
+package httpServer.http;
+
+public class RequestParser {
+
+    public static HTTPRequest parse(String rawRequest) {
+
+        String[] lines = rawRequest.split(Constants.CRLF);
+        for (int i = 0; i < lines.length; i++) {
+            System.out.print(lines[i]);
+        }
+
+        String[] requestLine = lines[0].split(" ");
+
+        HTTPRequest httpRequest = new HTTPRequest(requestLine[0], requestLine[1], requestLine[2]);
+        return httpRequest;
+    }
+
+}
