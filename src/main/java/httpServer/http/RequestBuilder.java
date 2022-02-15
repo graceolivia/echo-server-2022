255fa08c2a0eba581373eb3a3ae3a7cf0653902f
Grace <gracelidinskysmith@gmail.com>
2022-02-15 14:41:48 -0500

    Create methods for RequestBuilder

--- src/main/java/httpServer/http/RequestBuilder.java
+++ src/main/java/httpServer/http/RequestBuilder.java
@@ -1,18 +1,38 @@
 package httpServer.http;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RequestBuilder {
 
-    public static HTTPRequest parse(String rawRequest) {
+    public static String method;
+    public static String resource;
+    public static String httpVersionNumber;
+    public Map<String, String> headers = new HashMap<>();
+    public String body;
 
-        String[] lines = rawRequest.split(Constants.CRLF);
-        for (int i = 0; i < lines.length; i++) {
-            System.out.print(lines[i]);
-        }
 
-        String[] requestLine = lines[0].split(" ");
+    public RequestBuilder buildRequestLine(String line) {
+        String[] requestLine = line.split(" ");
+        this.method = requestLine[0];
+        this.resource = requestLine[1];
+        this.httpVersionNumber = requestLine[2];
+        return this;
+    }
 
-        HTTPRequest httpRequest = new HTTPRequest(requestLine[0], requestLine[1], requestLine[2]);
-        return httpRequest;
+    public RequestBuilder buildHeaderLine(String line) {
+        String[] requestLine = line.split(": ");
+        this.headers.put(requestLine[0], requestLine[1]);
+        return this;
+    }
+
+    public RequestBuilder setBody(String line) {
+        this.body = line;
+        return this;
+    }
+
+    public HTTPRequest build() {
+        return new HTTPRequest(method, resource, httpVersionNumber, headers, body);
     }
 
 }
