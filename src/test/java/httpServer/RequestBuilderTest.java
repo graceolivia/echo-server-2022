fdfd85fd0275dc124e38bcac9113aea259ffe90d
Grace <gracelidinskysmith@gmail.com>
2022-02-21 10:19:16 -0500

    Add test for RequestBuilder header

--- src/test/java/httpServer/RequestBuilderTest.java
+++ src/test/java/httpServer/RequestBuilderTest.java
@@ -25,6 +25,7 @@ public class RequestBuilderTest {
     void setUp() throws IOException {
         requestBuilder = new RequestBuilder();
         headers = new HashMap();
+        headers.put("Host", "localhost:5000");
         httpRequestExpected = new HTTPRequest("GET", "/", "HTTP/1.1", headers, null);
         request = "GET / HTTP/1.1" + Constants.crlf +
                 "Host: localhost:5000" + Constants.crlf +
@@ -33,13 +34,22 @@ public class RequestBuilderTest {
     }
 
     @Test
-    void testRequestParserReturnsCorrectHTTPResponseObject() throws IOException {
+    void testRequestBuilderCorrectlyParsesStatusLine() throws IOException {
         requestBuilder = requestBuilder.buildRequestLine("GET / HTTP/1.1");
 
         HTTPRequest httpParsed = requestBuilder.build();
         assertEquals(httpRequestExpected.method, httpParsed.method);
         assertEquals(httpRequestExpected.resource, httpParsed.resource);
         assertEquals(httpRequestExpected.httpVersionNumber, httpParsed.httpVersionNumber);
-
     }
+
+    @Test
+    void testRequestBuilderCorrectlyParsesHeader() throws IOException {
+        requestBuilder = requestBuilder.buildHeaderLine("Host: localhost:5000");
+
+        HTTPRequest httpParsed = requestBuilder.build();
+        assertEquals(httpRequestExpected.headers.get("Host"), httpParsed.headers.get("Host"));
+    }
+
+
 }
