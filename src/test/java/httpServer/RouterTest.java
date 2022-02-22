3a2bc30157ebc4784949143a7ee5ec4432524c8a
Grace <gracelidinskysmith@gmail.com>
2022-02-22 10:04:03 -0500

    Fix tests

--- src/test/java/httpServer/RouterTest.java
+++ src/test/java/httpServer/RouterTest.java
@@ -1,65 +1,73 @@
-//package httpServer;
-//
-//import httpServer.http.Constants;
-//import httpServer.http.request.HTTPRequest;
-//import httpServer.http.response.HTTPResponseBuilder;
-//import httpServer.routes.Router;
-//import httpServer.http.StatusCodes;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//
-//import java.io.IOException;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//public class RouterTest {
-//
-//    HTTPResponseBuilder responseBuilder;
-//    Router router;
-//
-//    @BeforeEach
-//    void setUp() throws IOException {
-//        responseBuilder = new HTTPResponseBuilder();
-//        router = new Router(HttpServer.getRoutes(), responseBuilder);
-//    }
-//    @Test
-//    void testRouterReturns200ForGetRequestToResourceWithGetAllowed() throws IOException {
-//        HTTPRequest request = new HTTPRequest("GET", "/simple_get", "HTTP/1.1");
-//        String expectedResponse = "HTTP/1.1 " + StatusCodes.OK.httpResponse + Constants.crlf + "Allow: GET" + Constants.crlf + "Content-Length: 0" + Constants.crlf;
-//        String returnedResponse = router.getResponse(request);
-//        assertEquals(expectedResponse, returnedResponse);
-//    }
-//
-//    @Test
-//    void testRouterReturns404ForNonExistentResource() throws IOException {
-//        HTTPRequest request = new HTTPRequest("GET", "/anything_invalid", "HTTP/1.1");
-//        String expectedResponse = "HTTP/1.1 " + StatusCodes.PAGE_NOT_FOUND.httpResponse + Constants.crlf + "Content-Length: 0" + Constants.crlf;
-//        String returnedResponse = router.getResponse(request);
-//        assertEquals(expectedResponse.toString(), returnedResponse);
-//    }
-//
-//    @Test
-//    void testRouterReturns405ForInvalidMethodRequestToExistingResource() throws IOException {
-//        HTTPRequest request = new HTTPRequest("GET", "/head_request", "HTTP/1.1");
-//        String expectedResponse = "HTTP/1.1 " + StatusCodes.NOT_ACCEPTABLE.httpResponse + Constants.crlf + "Allow: HEAD, OPTIONS" + Constants.crlf +  "Content-Length: 0" + Constants.crlf;;
-//        String returnedResponse = router.getResponse(request);
-//        assertEquals(expectedResponse, returnedResponse);
-//    }
-//
-//    @Test
-//    void testRouterReturns200ForFirstAllowedRequestTypeToExistingResourceWithMultipleMethods() throws IOException {
-//        HTTPRequest request = new HTTPRequest("OPTIONS", "/head_request", "HTTP/1.1");
-//        String expectedResponse = "HTTP/1.1 " + StatusCodes.OK.httpResponse + Constants.crlf + "Allow: HEAD, OPTIONS" + Constants.crlf +  "Content-Length: 0" + Constants.crlf;;
-//        String returnedResponse = router.getResponse(request);
-//        assertEquals(expectedResponse, returnedResponse);
-//    }
-//
-//    @Test
-//    void testRouterReturns200ForSecondAllowedRequestTypeToExistingResourceWithMultipleMethods() throws IOException {
-//        HTTPRequest request = new HTTPRequest("HEAD", "/head_request", "HTTP/1.1");
-//        String expectedResponse = "HTTP/1.1 " + StatusCodes.OK.httpResponse + Constants.crlf + "Allow: HEAD, OPTIONS" + Constants.crlf +  "Content-Length: 0" + Constants.crlf;;
-//        String returnedResponse = router.getResponse(request);
-//        assertEquals(expectedResponse, returnedResponse);
-//    }
-//
-//}
+package httpServer;
+
+import httpServer.http.Constants;
+import httpServer.http.request.HTTPRequest;
+import httpServer.http.response.HTTPResponse;
+import httpServer.http.response.HTTPResponseBuilder;
+import httpServer.routes.Router;
+import httpServer.http.StatusCodes;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RouterTest {
+
+    HTTPResponseBuilder responseBuilder;
+    Router router;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        responseBuilder = new HTTPResponseBuilder();
+        router = new Router(HttpServer.getRoutes(), responseBuilder);
+    }
+    @Test
+    void testRouterReturns200ForGetRequestToResourceWithGetAllowed() throws IOException {
+        Map<String, String> headers = new HashMap();
+        HTTPRequest request = new HTTPRequest("GET", "/simple_get", "HTTP/1.1", headers, null);
+        String expectedResponse = "HTTP/1.1 " + StatusCodes.OK.httpResponse + Constants.crlf + "Content-Length: 0" + Constants.crlf + "Content-Type: text/plain" + Constants.crlf + Constants.crlf;
+        String returnedResponse = router.getResponse(request).toString();
+        assertEquals(expectedResponse, returnedResponse);
+    }
+
+    @Test
+    void testRouterReturns404ForNonExistentResource() throws IOException {
+        Map<String, String> headers = new HashMap();
+        HTTPRequest request = new HTTPRequest("GET", "/anything_invalid", "HTTP/1.1", headers, null);
+        String expectedResponse = "HTTP/1.1 " + StatusCodes.PAGE_NOT_FOUND.httpResponse + Constants.crlf + "Content-Length: 0" + Constants.crlf + "Content-Type: text/plain" + Constants.crlf + Constants.crlf;
+        String returnedResponse = router.getResponse(request).toString();
+        assertEquals(expectedResponse, returnedResponse);
+    }
+
+    @Test
+    void testRouterReturns405ForInvalidMethodRequestToExistingResource() throws IOException {
+        Map<String, String> headers = new HashMap();
+        HTTPRequest request = new HTTPRequest("GET", "/head_request", "HTTP/1.1", headers, null);
+        String expectedResponse = "HTTP/1.1 " + StatusCodes.NOT_ACCEPTABLE.httpResponse + Constants.crlf + "Allow: HEAD, OPTIONS" + Constants.crlf +  "Content-Length: 0" + Constants.crlf;;
+        String returnedResponse = router.getResponse(request).toString();
+        assertEquals(expectedResponse, returnedResponse);
+    }
+
+    @Test
+    void testRouterReturns200ForFirstAllowedRequestTypeToExistingResourceWithMultipleMethods() throws IOException {
+        Map<String, String> headers = new HashMap();
+        HTTPRequest request = new HTTPRequest("OPTIONS", "/head_request", "HTTP/1.1", headers, null);
+        String expectedResponse = "HTTP/1.1 " + StatusCodes.OK.httpResponse + Constants.crlf +  "Content-Length: 0" + Constants.crlf + "Content-Type: text/plain" + Constants.crlf + Constants.crlf;
+        String returnedResponse = router.getResponse(request).toString();
+        assertEquals(expectedResponse, returnedResponse);
+    }
+
+    @Test
+    void testRouterReturns200ForSecondAllowedRequestTypeToExistingResourceWithMultipleMethods() throws IOException {
+        Map<String, String> headers = new HashMap();
+        HTTPRequest request = new HTTPRequest("HEAD", "/head_request", "HTTP/1.1", headers, null);
+        String expectedResponse = "HTTP/1.1 " + StatusCodes.OK.httpResponse + Constants.crlf +  "Content-Length: 0" + Constants.crlf + "Content-Type: text/plain" + Constants.crlf + Constants.crlf;
+        String returnedResponse = router.getResponse(request).toString();
+        assertEquals(expectedResponse, returnedResponse);
+    }
+
+}
